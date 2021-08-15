b6c78b9d7583cb33b71fe24453477f6eb3df43e7
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-15 10:39:48 -0300

    Estilizando component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import style from "./styles.module.scss"
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
-import { FaUniversity, FaInstagram, FaRocket } from "react-icons/fa"
+import { FaUniversity, FaInstagram } from "react-icons/fa"
+import { ImRocket } from "react-icons/im"
 
 export function Header() {
 
     return (
         <header className={style.headerContainer}>
 
-            <button><a href="#"><AiFillGithub size={30} /></a></button>
-            <button> <a href="#"><AiFillLinkedin size={30} /></a></button>
-            <button> <a href="#"><FaUniversity size={30} /></a></button>
-            <button><a href="#"><FaInstagram size={30} /></a></button>
-            <button> <a href="#"><FaRocket size={30} /></a></button>
+            <button><a target="_blank" href="https://github.com/Arthur-art"><AiFillGithub color={"white"} size={30} /></a></button>
+            <button> <a target="_blank" href="https://drive.google.com/drive/folders/1wNmToPgvdmG21I2Tc5fmxG-djWt2Sqmc"><FaUniversity color="white" size={30} /></a></button>
+            <button> <a target="_blank" href="https://app.rocketseat.com.br/me/arthur-teixeiraa-08541#"><ImRocket color="white" size={30} /></a></button>
+            <button> <a target="_blank" href="https://www.linkedin.com/in/arthur-teixeira-santos-silva-167189177/"><AiFillLinkedin color="white" size={30} /></a></button>
+            <button><a target="_blank" href="https://www.instagram.com/arthurteixeira.ss/"><FaInstagram color="white" size={30} /></a></button>
 
         </header>
     )
