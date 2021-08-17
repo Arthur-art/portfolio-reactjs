fb02ae83bda84801bcec626a57f354c130d3eae5
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-16 21:23:38 -0300

    Ajuste nas imagens

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Applications } from './components/Applications';
 import { Profile } from './components/Profile';
 import styles from "./app.module.scss"
+import CovidImg from "./img/Covidpwa.png"
+import scienceImg from "./img/scienceNet.png"
+import DevFinanceImg from "./img/DevFinance.png"
+import MoveitImg from "./img/Moveit.png"
 
 export function App() {
 
@@ -14,7 +18,7 @@ export function App() {
           description={"Nesta aplicação desenvolvi um layout inspirado no site da NETFLIX, com foco em filmes de ciências e ficção científica."}
           title={"Science-net"}
           alt={"img-netflix"}
-          img={"https://arthur-art-portfolio.vercel.app/src/img/ScienceNet.JPG"}
+          img={scienceImg}
           link={"https://repository-movie-created-arthur-art.vercel.app/"}
           subTitle="Layout inspirado nos sites de streaming" />
 
@@ -23,7 +27,7 @@ export function App() {
           description={"Esta aplicação é um projeto da Rocketseat a #NLW4-React.Projeto construido em react e next, que ensina a desenvolver um jogo no estilo pomodoro para estudar com eficiência e evitar o sedentarismo."}
           title={"Moveit"}
           alt={"img-moveit"}
-          img={"https://arthur-art-portfolio.vercel.app/src/img/Moveit.JPG"}
+          img={MoveitImg}
           link={"https://moveit-next-arthur-art.vercel.app/"}
           subTitle="Site estilo pomodoro" />
 
@@ -32,7 +36,7 @@ export function App() {
           description={"Esta aplicação é um projeto da Rocketseat o Discovery.Projeto que ensina a desenvolver um sistema de cálculo para finanças."}
           title={"DevFinance"}
           alt={"img-devfinance"}
-          img={"https://arthur-art-portfolio.vercel.app/src/img/DevFinance.JPG"}
+          img={DevFinanceImg}
           link={"https://dev-finance-created-arthur-art.vercel.app/"}
           subTitle="Organizando finanças" />
 
@@ -41,7 +45,7 @@ export function App() {
           description={"Nesta aplicação foi desenvolvido uma Progressive Web Application para mostrar os dados de uma api pública em uma interface trabalhada em reactJS, essa api disponibiliza dados de vários países sobre o COVID19."}
           title={"COVID-19-PWA"}
           alt={"img-covid"}
-          img={"https://arthur-art-portfolio.vercel.app/src/img/Covid.jpeg"}
+          img={CovidImg}
           link={"https://covid-pwa-arthur-art.vercel.app/"}
           subTitle="Informações sobre o COVID-19" />
       </div>
