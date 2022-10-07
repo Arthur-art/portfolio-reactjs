1b76069beb014624805edb758c167e721d964c4b
arthur-art <arthurteixeira.guts@gmail.com>
2022-10-07 16:43:11 -0300

    Adicionando um novo projeto

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,10 +3,11 @@ import scienceImg from '../img/scienceNet.png'
 import DevFinanceImg from '../img/DevFinance.png'
 import MoveitImg from '../img/Moveit.png'
 import igniteFeedImg from '../img/igniteFeed.png'
+import githubexplorer from '../img/githubexplorer.png'
 
 export const sites = [
   {
-    id:"Projeto1",
+    id: 'Projeto1',
     description:
       'Nesta aplicação desenvolvi um layout inspirado no site da NETFLIX, com foco em filmes de ciências e ficção científica.',
     title: 'Science-net',
@@ -16,7 +17,17 @@ export const sites = [
     subTitle: 'Layout inspirado nos sites de streaming',
   },
   {
-    id:"Projeto2",
+    id: 'Projeto2',
+    description:
+      'Nesta aplicação eu desenvolvi um frontend em react utilizando vite, para consumir uma api do github e trazendo os usuários de forma dinâmica.',
+    title: 'Github Explorer',
+    alt: 'img-github-explorer',
+    img: githubexplorer,
+    link: 'https://github-explorer-puce.vercel.app/',
+    subTitle: 'Frontend consumindo uma api do github',
+  },
+  {
+    id: 'Projeto3',
     description:
       'Esta aplicação é um projeto da Rocketseat a #NLW4-React.Projeto construido em react e next, que ensina a desenvolver um jogo no estilo pomodoro para estudar com eficiência e evitar o sedentarismo.',
     title: 'Moveit',
@@ -26,7 +37,7 @@ export const sites = [
     subTitle: 'Site estilo pomodoro',
   },
   {
-    id:"Projeto3",
+    id: 'Projeto4',
     description:
       'Esta aplicação é um projeto da Rocketseat o Discovery.Projeto que ensina a desenvolver um sistema de cálculo para finanças.',
     title: 'DevFinance',
@@ -36,7 +47,7 @@ export const sites = [
     subTitle: 'Organizando finanças',
   },
   {
-    id:"Projeto4",
+    id: 'Projeto5',
     description:
       'Esta aplicação é um projeto da Rocketseat o Ignite-ReactJS, Projeto com ensina a desenvolver o frontend de um feed numa rede social.',
     title: 'Ignite-feed',
@@ -46,7 +57,7 @@ export const sites = [
     subTitle: 'Aplicação para simular um feed de uma rede social',
   },
   {
-    id:"Projeto5",
+    id: 'Projeto6',
     description:
       'Nesta aplicação foi desenvolvido uma Progressive Web Application para mostrar os dados de uma api pública em uma interface trabalhada em reactJS, essa api disponibiliza dados de vários países sobre o COVID19.',
     title: 'COVID-19-PWA',
